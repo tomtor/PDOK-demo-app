453707b12a1c4b60acceda23c0948d36c09eb00e
Tom Vijlbrief <tvijlbrief@gmail.com>
2016-02-16 18:47:16 +0100

    Add timestamp to metadata tables

--- server.py
+++ server.py
@@ -20,9 +20,9 @@ c = conn.cursor()
 
 # Create tables
 c.execute('''CREATE TABLE IF NOT EXISTS requests
-             (path text);''')
+             (path text, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 c.execute('''CREATE TABLE IF NOT EXISTS datasets
-             (privateKey text, publicKey text, activated integer);''')
+             (privateKey text, publicKey text, activated integer, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 c.execute('''CREATE TABLE IF NOT EXISTS d_d89d5ee2d34711e59d78bcaec5c2cce2
              (uuid text, privUuid text, ip text, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, location text);''')
 c.execute('''INSERT INTO datasets (privateKey, publicKey, activated) VALUES("d89d5ee2-d347-11e5-9d78-bcaec5c2cce2", "d89d5ee2-d347-11e5-9d78-bcaec5c2cce2", 1);''')
