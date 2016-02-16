51a07074e7994ad9192cb2a1e3c742fd46335174
Tom Vijlbrief <tvijlbrief@gmail.com>
2016-02-16 18:58:00 +0100

    Add email to registration database

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS requests
              (path text, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 c.execute('''CREATE TABLE IF NOT EXISTS datasets
-             (privateKey text, publicKey text, activated integer, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
+             (privateKey text, publicKey text, activated integer, email text, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);''')
 c.execute('''CREATE TABLE IF NOT EXISTS d_d89d5ee2d34711e59d78bcaec5c2cce2
              (uuid text, privUuid text, ip text, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, location text);''')
 c.execute('''INSERT INTO datasets (privateKey, publicKey, activated) VALUES("d89d5ee2-d347-11e5-9d78-bcaec5c2cce2", "d89d5ee2-d347-11e5-9d78-bcaec5c2cce2", 1);''')
@@ -162,7 +162,7 @@ class MyServer(BaseHTTPRequestHandler):
             s = smtplib.SMTP('localhost')
             s.send_message(msg)
             s.quit()
-            c.execute("INSERT INTO datasets (privateKey, publicKey, activated) VALUES(?, ?, 0);", (privateKey, publicKey) )
+            c.execute("INSERT INTO datasets (privateKey, publicKey, activated, email) VALUES(?, ?, 0, ?);", (privateKey, publicKey, mail) )
             self.wfile.write(bytes("true", "utf-8"))
 
         elif q[2] == 'activate' :
